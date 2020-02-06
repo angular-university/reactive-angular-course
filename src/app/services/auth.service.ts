72c57f0ccf2e07f2b4aafe2cd864ce6405926bf6
JHades <jhades.dev@gmail.com>
2020-02-06 16:40:09 +0100

    Reactive Angular with RxJs Course

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,9 +4,11 @@ import {User} from '../model/user';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {filter, shareReplay, tap, map} from 'rxjs/operators';
 
+const AUTH_DATA = "auth_data";
+
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class AuthService {
 
@@ -17,7 +19,15 @@ export class AuthService {
   isLoggedOut$: Observable<boolean>;
 
   constructor(private http: HttpClient) {
+
     this.user$ = this.subject.asObservable();
+
+    const user = localStorage.getItem(AUTH_DATA);
+
+    if (user) {
+      this.subject.next(JSON.parse(user));
+    }
+
     this.isLoggedIn$ = this.user$.pipe(map(user => !!user));
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
   }
@@ -26,13 +36,17 @@ export class AuthService {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     return this.http.post<User>('/api/login', {email, password}, {headers})
       .pipe(
-        tap(user => this.subject.next(user)),
+        tap(user => {
+          localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+          this.subject.next(user);
+        }),
         shareReplay()
       );
   }
 
   logout() {
     this.subject.next(null);
+    localStorage.removeItem(AUTH_DATA);
   }
 
 
