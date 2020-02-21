e3145a31a523a2b3cba449448d908cc67a405596
Your Name <you@example.com>
2020-02-21 15:30:56 +0100

    Reactive Angular Course

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,8 +20,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
   constructor(
-      private coursesService:CoursesService,
-      private dialog: MatDialog) {
+      private coursesService:CoursesService) {
 
   }
 
@@ -41,20 +40,6 @@ export class HomeComponent implements OnInit {
           .pipe(
               map(courses => courses.filter(course => course.category == "ADVANCED"))
           );
-  }
-
-  editCourse(course: Course) {
-
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
   }
 
 }
