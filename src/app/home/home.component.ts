4aaf976107c51a81fb71cd8b0418ec8d9f9846ce
Your Name <you@example.com>
2020-02-21 14:16:19 +0100

    Reactive Angular Course

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareRep
 import {HttpClient} from '@angular/common/http';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
+import {CoursesService} from '../services/courses.service';
 
 
 @Component({
@@ -14,29 +15,32 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 })
 export class HomeComponent implements OnInit {
 
-  beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
-
-  constructor(private http: HttpClient, private dialog: MatDialog) {
+  constructor(
+      private coursesService:CoursesService,
+      private dialog: MatDialog) {
 
   }
 
   ngOnInit() {
 
-    this.http.get('/api/courses')
-      .subscribe(
-        res => {
+        const courses$ = this.coursesService.loadAllCourses()
+            .pipe(
+                map(courses => courses.sort(sortCoursesBySeqNo))
+            );
 
-          const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
+        this.beginnerCourses$ = courses$
+            .pipe(
+              map(courses => courses.filter(course => course.category == "BEGINNER"))
+            );
 
-          this.beginnerCourses = courses.filter(course => course.category == "BEGINNER");
-
-          this.advancedCourses = courses.filter(course => course.category == "ADVANCED");
-
-        });
-
+      this.advancedCourses$ = courses$
+          .pipe(
+              map(courses => courses.filter(course => course.category == "ADVANCED"))
+          );
   }
 
   editCourse(course: Course) {
@@ -54,7 +58,5 @@ export class HomeComponent implements OnInit {
   }
 
 }
-
-
 
 
---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Course} from '../model/course';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+
+@Injectable({
+    providedIn:'root'
+})
+export class CoursesService {
+
+    constructor(private http:HttpClient) {
+
+    }
+
+    loadAllCourses(): Observable<Course[]> {
+        return this.http.get<Course[]>("/api/courses")
+            .pipe(
+                map(res => res["payload"])
+            );
+    }
+
+
+}
