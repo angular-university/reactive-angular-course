2bf289a723f2a250a3d089050e7965004a7903af
Your Name <you@example.com>
2020-02-21 14:48:34 +0100

    Reactive Angular Course

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Course} from '../model/course';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {map, shareReplay} from 'rxjs/operators';
 
 
 @Injectable({
@@ -17,7 +17,8 @@ export class CoursesService {
     loadAllCourses(): Observable<Course[]> {
         return this.http.get<Course[]>("/api/courses")
             .pipe(
-                map(res => res["payload"])
+                map(res => res["payload"]),
+                shareReplay()
             );
     }
 
