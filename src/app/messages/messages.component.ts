0c2e4add3c725660332f9cc11fbcbcc481bc95e8
Your Name <you@example.com>
2020-03-06 16:19:21 +0100

    Reactive Angular Course

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {Message} from '../model/message';
 import {tap} from 'rxjs/operators';
+import {MessagesService} from './messages.service';
 
 @Component({
   selector: 'messages',
@@ -12,13 +13,20 @@ export class MessagesComponent implements OnInit {
 
   showMessages = false;
 
+  errors$: Observable<string[]>;
 
-  constructor() {
+
+  constructor(public messagesService: MessagesService) {
+
+      console.log("Created messages component");
 
   }
 
   ngOnInit() {
-
+      this.errors$ = this.messagesService.errors$
+          .pipe(
+              tap(() => this.showMessages = true)
+          );
 
   }
 
--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,12 +1,20 @@
 import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
 
 @Injectable()
 export class MessagesService {
 
+    private subject = new BehaviorSubject<string[]>([]);
+
+    errors$: Observable<string[]> = this.subject.asObservable()
+        .pipe(
+            filter(messages => messages && messages.length > 0)
+        );
 
     showErrors(...errors: string[]) {
-
+        this.subject.next(errors);
     }
 
 }
