f67bce09294140145c03c9e8476633dfb52cc1fc
Your Name <you@example.com>
2020-03-13 14:15:42 +0100

    Reactive Angular Course

--- server/save-course.route.ts
+++ server/save-course.route.ts
@@ -5,11 +5,11 @@ import {setTimeout} from 'timers';
 
 export function saveCourse(req: Request, res: Response) {
 
-
+/*
   console.log("ERROR saving course!");
   res.sendStatus(500);
   return;
-
+*/
 
 
     const id = req.params["id"],
