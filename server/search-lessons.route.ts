1c94cda4fae0210744f08326131f00dde26af5e5
Your Name <you@example.com>
2020-03-27 15:14:58 +0100

    Reactive Angular Course

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -17,7 +17,14 @@ export function searchLessons(req: Request, res: Response) {
           pageNumber = parseInt(queryParams.pageNumber) || 0,
           pageSize = parseInt(queryParams.pageSize) || 3;
 
-    let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
+    let lessons;
+
+    if (courseId) {
+     lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
+    }
+    else {
+        lessons = Object.values(LESSONS);
+    }
 
     if (filter) {
        lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
