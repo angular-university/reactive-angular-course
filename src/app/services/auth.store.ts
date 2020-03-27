3b8356a0181d05ef9b9efb46f1e49de89dfbbc47
Your Name <you@example.com>
2020-03-27 14:22:02 +0100

    Reactive Angular Course

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,7 @@ import {User} from '../model/user';
 import {map, shareReplay, tap} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
 
+const AUTH_DATA = "auth_data";
 
 @Injectable({
     providedIn: 'root'
@@ -23,18 +24,28 @@ export class AuthStore {
 
         this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
 
+        const user = localStorage.getItem(AUTH_DATA);
+
+        if (user) {
+            this.subject.next(JSON.parse(user));
+        }
+
     }
 
     login(email:string, password:string): Observable<User> {
         return this.http.post<User>("/api/login", {email, password})
             .pipe(
-                tap(user => this.subject.next(user)),
+                tap(user => {
+                    this.subject.next(user);
+                    localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+                }),
                 shareReplay()
             );
     }
 
     logout() {
         this.subject.next(null);
+        localStorage.removeItem(AUTH_DATA);
     }
 
 
