0436cc3ca9e69e4a88fb9067a85566c401e12e0a
Your Name <you@example.com>
2020-04-10 14:57:56 +0200

    Reactive Angular Course

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,9 +13,15 @@ import {
   withLatestFrom,
   concatAll, shareReplay, catchError
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
+import {merge, fromEvent, Observable, concat, throwError, combineLatest} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import {CoursesService} from '../services/courses.service';
+
+
+interface CourseData {
+    course: Course;
+    lessons: Lesson[];
+}
 
 
 @Component({
@@ -25,9 +31,8 @@ import {CoursesService} from '../services/courses.service';
 })
 export class CourseComponent implements OnInit {
 
-  course$: Observable<Course>;
+  data$: Observable<CourseData>;
 
-  lessons$: Observable<Lesson[]>;
 
   constructor(private route: ActivatedRoute,
               private coursesService: CoursesService) {
@@ -39,9 +44,21 @@ export class CourseComponent implements OnInit {
 
         const courseId = parseInt(this.route.snapshot.paramMap.get("courseId"));
 
-        this.course$ = this.coursesService.loadCourseById(courseId);
+        const course$ = this.coursesService.loadCourseById(courseId);
 
-        this.lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+        const lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+
+        this.data$ = combineLatest([course$, lessons$])
+            .pipe(
+                map(([course, lessons]) => {
+                    return {
+                        course,
+                        lessons
+                    }
+                }),
+                tap(console.log)
+            );
+
 
   }
 
