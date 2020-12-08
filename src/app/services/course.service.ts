d87f1cfb5d159b2625883b9fabc0fcad8b14d04a
Berker Erol <berker.erol@onespan.com>
2020-12-08 14:47:01 -0500

    refactored home component in reactive style and added a loader service.

---
+++ src/app/services/course.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from "../model/course";
+import { map } from 'rxjs/operators';
+
+
+
+@Injectable({providedIn: 'root'})
+export class CourseService {
+  constructor(private http: HttpClient) { }
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>("/api/courses")
+      .pipe(
+        map( res => res["payload"] )
+      );
+  }
+
+
+}
+
